ba5f3960c0ff3ca683ee038279f15f89a3e53036
Adrian MGL <adrienguzmanlopez@gmail.com>
2022-06-30 01:36:55 -0500

    App pokeapi v1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Pokeapi from "./Components/Pokeapi";
+
+function App() {
+  return (
+    <div className="App">
+      <Pokeapi />
+    </div>
+  );
+}
+
+export default App;
